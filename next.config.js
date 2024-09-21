9342fa2b475e809342454bd0132c9cf0bd90769d
qianweishuo <qianweishuo@bytedance.com>
2024-09-21 10:39:38 +0800

    solve tailwind directive warning and css format

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/rising-repo',
+  output: 'export', // 生成静态HTML输出，适用于静态托管
+  basePath: '/rising-repo', // 设置基本路径，所有页面和资源都将从此路径开始
   images: { unoptimized: true },
   logging: { fetches: { fullUrl: true } },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    '@app/**/*.{js,ts,jsx,tsx,mdx,css}',
+    '@pages/**/*.{js,ts,jsx,tsx,mdx,css}',
+    '@components/**/*.{js,ts,jsx,tsx,mdx,css}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
